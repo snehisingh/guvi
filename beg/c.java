b615bf78c03370687d8a49bd915bf2cdc012d66f
Snehil Singh <35743612+snehisingh@users.noreply.github.com>
2019-03-20 16:22:30 +0530

    Update c.java

--- beg/c.java
+++ beg/c.java
@@ -2,6 +2,21 @@ import java.util.*;
 
 public class{
     public public static void main() {
+        Scanner sc=sc.nextInt();
+        int a=sc.nextInt();
+        if(a%2==0)
+           {
+               System.out.println("Positive");
+           }
+           else if(a%2==0)
+           {
+               System.out.println("Negetive");
+           }
+        else
+        {
+            System.out.println("Zero");
+        }
+        
         
     }
 }
