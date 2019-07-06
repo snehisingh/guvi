fb3207cec34e9fb6424fbed2317034f7ed31fa87
Snehil Singh <35743612+snehisingh@users.noreply.github.com>
2019-07-06 09:53:55 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class pos
+{
+public static void mian(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n==0)
+{
+System.out.println("Zero");
+if(n%2==0)
+{
+System.out.print("Positive");
+}
+if(n%2!=0)
+{
+System.out.print("Negetive");
+}
+}
+}
