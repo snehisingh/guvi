4af35850106a1e7f10b3c053b9041dde20a47c33
Snehil Singh <35743612+snehisingh@users.noreply.github.com>
2019-07-13 00:47:31 +0530

    Update seven.java
    pro7

--- seven.java
+++ seven.java
@@ -3,11 +3,19 @@ public class seven
 {
     public static void main(String args[])
     {
-        int a=1;
-        int b=0;
+        int k=2;
+        int j=1;
+        int a,b;
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        if(n%2!=0)
+        while(j<n)
+        {
+            j=j*k;
+        }
+        a=j-n;
+        j=j/2;
+        b=n-j;
+        if(a<b)
         {
             System.out.print(a);
         }
