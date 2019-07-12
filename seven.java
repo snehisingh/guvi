8569218d9d9db61aad7e72fa2cc3e5652bdaabc5
Snehil Singh <35743612+snehisingh@users.noreply.github.com>
2019-07-13 00:29:21 +0530

    Create seven.java

---
+++ seven.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class seven
+{
+    public static void main(String args[])
+    {
+        int a=1;
+        int b=0;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2!=0)
+        {
+            System.out.print(a);
+        }
+        else
+        {
+            System.out.print(b);
+        }
+    }
+}
